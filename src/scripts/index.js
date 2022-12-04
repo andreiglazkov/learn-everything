5952345c748ce707832143a05da5eddf73200396
Andrei Glazkov <andrei.glazkkov@gmail.com>
2022-12-04 21:08:31 +0100

    - some js experiments

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,12 @@
 function clickOnButton() {
     alert("Hallo, Welt!");
 }
+
+var message = 'I said what\'s up?';
+function updateMessage() {
+    var el = document.getElementById('header-left-side');
+
+    var name = prompt('Enter your name: ').valueOf();
+    el.addEventListener("", el.innerText('Hallo, ' + name + '!'));
+}
+updateMessage();
